dae38312c7ccc65b93c386a450f432eab77eeb7a
Fonefixer <dgbarrett@gmail.com>
2017-03-16 08:35:16 +0000

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -9,7 +9,6 @@ function update(widget_id, time_format, date_format) {
   var weekdays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
   var $date = jQuery("#" + widget_id + " .date");
   var $time = jQuery("#" + widget_id + " .time");
-  
 
   // Date
   if (date_format != "none") {
@@ -17,7 +16,7 @@ function update(widget_id, time_format, date_format) {
     var year = currentTime.getFullYear();
     var month = currentTime.getMonth();
     var day = currentTime.getDate();
-    var weekday = currentTime.getDay();
+	var weekday = currentTime.getDay();
 
     if (date_format == "long") {
       $date.text(months[month] + " " + day + ", " + year);
@@ -31,11 +30,29 @@ function update(widget_id, time_format, date_format) {
     else if (date_format == "european") {
       $date.text(day + "/" + (month + 1) + "/" + year);
     }
-    else if (date_format == "full") {
+	else if (date_format == "full") {
 	   $date.text(weekdays[weekday] + " " + day + " " + months[month] + " " + year);
-	  }
-	  else if (date_format == "longw") {
+	}
+	else if (date_format == "longw") {
       $date.text(weekdays[weekday] + " " + months[month] + " " + day + ", " + year);
+    }
+	 else if (date_format == "mediumny") {
+      $date.text(months[month].substring(0, 3) + " " + day);
+    }
+	  if (date_format == "longny") {
+      $date.text(months[month] + " " + day);
+    }
+	else if (date_format == "fullny") {
+	   $date.text(weekdays[weekday] + " " + day + " " + months[month]);
+	}
+	else if (date_format == "longwny") {
+      $date.text(weekdays[weekday] + " " + months[month] + " " + day);
+    }
+	else if (date_format == "longr") {
+      $date.text(day + ", " + months[month] + " " + year);
+    }
+    else if (date_format == "mediumr") {
+      $date.text(day + " " + months[month].substring(0, 3) + " " + year);
     }
   }
 
